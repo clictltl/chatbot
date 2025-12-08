dad9b304e3cc6069662d2d53e0004fa9c10f7fc3
nathan <nathanrab@gmail.com>
2025-12-08 09:56:58 -0300

    fix vite 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: mode === 'production' ? '/chatbot/' : './',
+  base: mode === 'production' ? 'chatbot' : './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
