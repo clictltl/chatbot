e0e0adb4dfbabaf8cca4cb58cf1534091f640351
Lucas Coletti <lcoletti@gmail.com>
2025-12-11 21:44:17 -0300

    Test to fix github pages

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { checkLogin } from './auth';
 
-checkLogin().then(() => {
-  createApp(App).mount('#app');
-});
+createApp(App).mount('#app');
+
+// checkLogin().then(() => {
+//   createApp(App).mount('#app');
+// });
